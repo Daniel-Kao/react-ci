500427aa32825174246bda81c5a397bcf41712de
daniel <daniel@192.168.124.14>
2020-02-14 08:34:36 +0800

    travis setup
    Signed-off-by: daniel <daniel@192.168.124.14>

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>and sav1123 asdreload.</p>
+        <p>and sav1123 asdreloadasd.</p>
       </header>
     </div>
   );
